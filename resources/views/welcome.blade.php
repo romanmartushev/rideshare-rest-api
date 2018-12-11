07df20dfbd5fe8987167602d0d405de1cd9611ae
Roman <romanm@paulbunyan.net>
2018-12-11 12:44:08 -0600

    add authenticated and password to clients table force migrations when pushing to production

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>RideShare Api Endpoints</title>
-
-        <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
